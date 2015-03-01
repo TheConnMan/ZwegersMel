d5e585500cce8303f34b32937bb39fbebb6d670f
TheConnMan <bcconn2112@gmail.com>
2015-02-28 19:12:31 -0500

    Changed app name. Moved usernames to UserConfig.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,12 +10,18 @@ class BootStrap {
 	}
 
     def init = { servletContext ->
+		def loc = ['../UserConfig.groovy', 'webapps/ROOT/Jenkins.groovy'].grep { new File(it).exists() }.first();
+		def localConfig = new ConfigSlurper().parse(new File(loc).toURI().toURL());
+		
 		println 'Bootstrapping'
-		def adminRole = new Role(authority: "ROLE_ADMIN").save()
-		def userRole = new Role(authority: "ROLE_USER").save()
-		createUser('admin', 'theconnman', adminRole)
-		createUser('user', 'theconnman', userRole)
+		new Role(authority: "ROLE_ADMIN").save();
+		new Role(authority: "ROLE_USER").save();
+		
+		localConfig.zwegersmel.users.each {
+			createUser(it.username, it.password, Role.findByAuthority(it.role));
+		}
     }
+	
     def destroy = {
 		
     }
